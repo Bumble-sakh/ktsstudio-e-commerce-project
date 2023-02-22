5586a1f0518f097b4c714d09175f6086ad3f77e6
Bumble <bumble.sakh@gmail.com>
2023-02-22 15:08:59 +1100

    refactor: header styles

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,28 +1,37 @@
+import logo from '@assets/images/logo.svg';
 import { Link } from 'react-router-dom';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
-    <header>
-      <div>Logo</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Products</Link>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </div>
+      <nav className={styles.navigation}>
+        <ul className={styles.list}>
+          <li className={`${styles.list__item} ${styles.list__item_active}`}>
+            <Link to="/" className={styles.navigation__link}>
+              Products
+            </Link>
           </li>
-          <li>
-            <Link to="/categories">Categories</Link>
+          <li className={styles.list__item}>
+            <Link to="/categories" className={styles.navigation__link}>
+              Categories
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About Us</Link>
+          <li className={styles.list__item}>
+            <Link to="/about" className={styles.navigation__link}>
+              About Us
+            </Link>
           </li>
         </ul>
       </nav>
-      <div>
-        <ul>
-          <li>Cart</li>
-          <li>User</li>
+      <div className={styles.user}>
+        <ul className={styles.list}>
+          <li className={styles.list__item}>Cart</li>
+          <li className={styles.list__item}>User</li>
         </ul>
       </div>
     </header>

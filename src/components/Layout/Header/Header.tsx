1471ee2c21a9b386a8d7609a610e2bd644e6bdc5
Bumble <bumble.sakh@gmail.com>
2023-02-22 18:43:23 +1100

    refactor: header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,38 +1,76 @@
+import cart from '@assets/images/cart.svg';
 import logo from '@assets/images/logo.svg';
-import { Link } from 'react-router-dom';
+import user from '@assets/images/user.svg';
+import ROUTES from '@config/routes';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src={logo} alt="logo" />
-      </div>
-      <nav className={styles.navigation}>
-        <ul className={styles.list}>
-          <li className={`${styles.list__item} ${styles.list__item_active}`}>
-            <Link to="/" className={styles.navigation__link}>
-              Products
-            </Link>
-          </li>
-          <li className={styles.list__item}>
-            <Link to="/categories" className={styles.navigation__link}>
-              Categories
-            </Link>
-          </li>
-          <li className={styles.list__item}>
-            <Link to="/about" className={styles.navigation__link}>
-              About Us
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.user}>
-        <ul className={styles.list}>
-          <li className={styles.list__item}>Cart</li>
-          <li className={styles.list__item}>User</li>
-        </ul>
+      <div className={`${styles.header__wrapper} wrapper`}>
+        <div className={styles.logo}>
+          <img src={logo} alt="logo" />
+        </div>
+
+        <nav className={styles.navigation}>
+          <ul className={styles.list}>
+            <li className={`${styles.list__item}`}>
+              <NavLink
+                to={ROUTES.index}
+                className={({ isActive }) =>
+                  classNames({
+                    [styles.navigation__link]: true,
+                    [styles.navigation__link_active]: isActive,
+                  })
+                }
+              >
+                Products
+              </NavLink>
+            </li>
+
+            <li className={styles.list__item}>
+              <NavLink
+                to={ROUTES.categories}
+                className={({ isActive }) =>
+                  classNames({
+                    [styles.navigation__link]: true,
+                    [styles.navigation__link_active]: isActive,
+                  })
+                }
+              >
+                Categories
+              </NavLink>
+            </li>
+
+            <li className={styles.list__item}>
+              <NavLink
+                to={ROUTES.about}
+                className={({ isActive }) =>
+                  classNames({
+                    [styles.navigation__link]: true,
+                    [styles.navigation__link_active]: isActive,
+                  })
+                }
+              >
+                About Us
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.user}>
+          <ul className={styles.list}>
+            <li className={styles.list__item}>
+              <img src={cart} alt="cart" />
+            </li>
+            <li className={styles.list__item}>
+              <img src={user} alt="user" />
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );

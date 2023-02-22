44379761e26c03c9e7f935f7189968ec94100517
Bumble <bumble.sakh@gmail.com>
2023-02-22 15:08:15 +1100

    fix: styles file name

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import styles from './styles/index.module.scss';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

e014a9bdd7d9ab942ea611ef442acf3402db5b75
Bumble <bumble.sakh@gmail.com>
2023-02-26 08:03:57 +1100

    feat: add burger

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,13 +1,32 @@
-import cart from '@assets/images/cart.svg';
+import { useEffect, useState } from 'react';
+
 import logo from '@assets/images/logo.svg';
-import user from '@assets/images/user.svg';
-import ROUTES from '@config/routes';
-import classNames from 'classnames';
-import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.scss';
+import Burger from '../Burger';
+import Navigation from '../Navigation';
+import User from '../User';
 
 const Header = () => {
+  const [isBurger, setIsBurger] = useState(false);
+
+  const resizeHandler = () => {
+    if (document.body.clientWidth <= 880) {
+      setIsBurger(true);
+    } else {
+      setIsBurger(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={`${styles.header__wrapper} wrapper`}>
@@ -15,62 +34,17 @@ const Header = () => {
           <img src={logo} alt="logo" />
         </div>
 
-        <nav className={styles.navigation}>
-          <ul className={styles.list}>
-            <li className={`${styles.list__item}`}>
-              <NavLink
-                to={ROUTES.index}
-                className={({ isActive }) =>
-                  classNames({
-                    [styles.navigation__link]: true,
-                    [styles.navigation__link_active]: isActive,
-                  })
-                }
-              >
-                Products
-              </NavLink>
-            </li>
-
-            <li className={styles.list__item}>
-              <NavLink
-                to={ROUTES.categories}
-                className={({ isActive }) =>
-                  classNames({
-                    [styles.navigation__link]: true,
-                    [styles.navigation__link_active]: isActive,
-                  })
-                }
-              >
-                Categories
-              </NavLink>
-            </li>
-
-            <li className={styles.list__item}>
-              <NavLink
-                to={ROUTES.about}
-                className={({ isActive }) =>
-                  classNames({
-                    [styles.navigation__link]: true,
-                    [styles.navigation__link_active]: isActive,
-                  })
-                }
-              >
-                About Us
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        <div className={styles.user}>
-          <ul className={styles.list}>
-            <li className={styles.list__item}>
-              <img src={cart} alt="cart" />
-            </li>
-            <li className={styles.list__item}>
-              <img src={user} alt="user" />
-            </li>
-          </ul>
-        </div>
+        {isBurger ? (
+          <Burger>
+            <Navigation />
+            <User />
+          </Burger>
+        ) : (
+          <>
+            <Navigation />
+            <User />
+          </>
+        )}
       </div>
     </header>
   );

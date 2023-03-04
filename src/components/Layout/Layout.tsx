0d8ea2bf6d77ea6f450009cf43a91bb195d89a57
Bumble <bumble.sakh@gmail.com>
2023-03-05 01:05:12 +1100

    feat: add useQueryParamsStoreInit to App

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,10 @@
 import Header from '@components/Layout/Header';
+import { useQueryParamsStoreInit } from '@store/RootStore/hooks';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
+  useQueryParamsStoreInit();
+
   return (
     <>
       <Header />

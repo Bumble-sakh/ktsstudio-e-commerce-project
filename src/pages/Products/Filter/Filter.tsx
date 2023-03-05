79e5fe93d05cb030280992068286053e2ded28d5
Bumble <bumble.sakh@gmail.com>
2023-03-06 02:28:06 +1100

    feat: add FilterStore

--- src/pages/Products/Filter/Filter.tsx
+++ src/pages/Products/Filter/Filter.tsx
@@ -1,35 +1,33 @@
-import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 import filter from '@assets/images/filter.svg';
+import CategoriesStore from '@store/CategoriesStore';
+import FilterStore from '@store/FilterStore';
+import { CategoryModel } from '@store/models/category';
+import PaginationStore from '@store/PaginationStore';
+import { useLocalStore } from '@utils/useLocalStore';
 import classNames from 'classnames';
+import { observer } from 'mobx-react-lite';
 import { useSearchParams } from 'react-router-dom';
 
 import styles from './Filter.module.scss';
-import { Category } from '../Products';
 
-type FilterProps = {
-  categories: Category[];
-  categoryId: number | null;
-  setCategoryId: React.Dispatch<React.SetStateAction<number | null>>;
-  setPaginationPage: React.Dispatch<React.SetStateAction<number>>;
-};
+const Filter = () => {
+  const categoriesStore = useLocalStore(() => new CategoriesStore());
+  const paginationStore = useLocalStore(() => new PaginationStore());
+  const filterStore = useLocalStore(() => new FilterStore());
 
-const Filter: React.FC<FilterProps> = ({
-  categories,
-  categoryId,
-  setCategoryId,
-  setPaginationPage,
-}) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [optionsIsVisible, setOptionsIsVisible] = useState(false);
+
+  useEffect(() => {
+    categoriesStore.getCategories();
+  }, [categoriesStore]);
 
   const selectOnClickHandler = () => {
-    setOptionsIsVisible((prev) => !prev);
+    filterStore.toggleOptionsIsVisible();
   };
 
-  const optionOnClickHandler = (category: Category) => {
-    setCategoryId(category.id);
-
+  const optionOnClickHandler = (category: CategoryModel) => {
     if (category.id) {
       searchParams.set('categoryId', String(category.id));
       setSearchParams(searchParams);
@@ -38,15 +36,15 @@ const Filter: React.FC<FilterProps> = ({
       setSearchParams(searchParams);
     }
 
-    setPaginationPage(1);
+    paginationStore.setPaginationPage(1);
     searchParams.delete('page');
     setSearchParams(searchParams);
 
     selectOnClickHandler();
   };
 
-  const options = categories.map((category) => {
-    const isSelected = categoryId === category.id;
+  const options = categoriesStore.categories.map((category) => {
+    const isSelected = filterStore.categoryId === category.id;
 
     const classes = classNames({
       [styles.list__item]: true,
@@ -70,7 +68,7 @@ const Filter: React.FC<FilterProps> = ({
         <img src={filter} alt="filter" />
         Filter
       </div>
-      {optionsIsVisible && (
+      {filterStore.optionsIsVisible && (
         <div className={styles.filter__options}>
           <ul className={styles.list}>{options}</ul>
         </div>
@@ -79,4 +77,4 @@ const Filter: React.FC<FilterProps> = ({
   );
 };
 
-export default Filter;
+export default observer(Filter);

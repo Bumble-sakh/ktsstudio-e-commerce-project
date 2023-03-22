7b5728a7bf10042a065ee99c19fb0c921b15bc5b
Bumble <bumble.sakh@gmail.com>
2023-03-23 02:17:07 +1100

    fix: rootStore

--- src/store/RootStore/RootStore.ts
+++ src/store/RootStore/RootStore.ts
@@ -1,9 +1,7 @@
-import BuyNowStore from '@store/RootStore/BuyNowStore';
 import CartStore from '@store/RootStore/CartStore';
 import QueryParamsStore from '@store/RootStore/QueryPramsStore';
 
 export default class RootStore {
   readonly queryParamsStore = new QueryParamsStore();
   readonly cartStore = new CartStore();
-  readonly buyNowStore = new BuyNowStore();
 }
